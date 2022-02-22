0d1114b40bc0e2229e096ca178085039b291011f
TuanLAT2000 <tuanlat2000@gmail.com>
2022-02-22 13:53:10 +0700

    git commit -m""

--- test/com/tuan/mathutil/test/MathUtilDDTTest.java
+++ test/com/tuan/mathutil/test/MathUtilDDTTest.java
@@ -19,7 +19,7 @@ import org.junit.runners.Parameterized.Parameters;
  */
 
 @RunWith(value = Parameterized.class)
-public class MathUtilTDDTest {
+public class MathUtilDDTTest {
     
     //0
     @Parameters
